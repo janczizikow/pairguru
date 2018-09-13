93a622e7b969326835ad5649e885e85d29012814
Jan Czizikow <jan.czizikow@gmail.com>
2018-09-13 20:26:12 +0200

    Fix: Comment model validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :movie
 
-  validates :user, :movie, uniqueness: true
+  validates :user, :movie, presence: true, uniqueness: { scope: %i[movie user] }
   validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
 end
