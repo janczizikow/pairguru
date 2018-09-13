e6afa6334d0997ef047204eba07b6c9a343ef9c5
Jan Czizikow <jan.czizikow@gmail.com>
2018-09-13 21:38:35 +0200

    Add CommentDecorator tests

--- spec/decorators/comment_decorator_spec.rb
+++ spec/decorators/comment_decorator_spec.rb
@@ -1,4 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe CommentDecorator do
+  let(:comment) { create(:comment).decorate }
+
+  it "returns date in formatted date" do
+    expect(comment.created_at).to eq Time.zone.now.strftime("%d %b,  %Y")
+  end
 end
