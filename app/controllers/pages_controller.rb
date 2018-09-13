c8aae463f92ca6d84314bd83a025b4cc60afd1e1
Jan Czizikow <jan.czizikow@gmail.com>
2018-09-14 01:17:50 +0200

    Add leaderboard logic & view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def comment_leaderboard
+    @users = User.select("name, COUNT(*) AS comments_count")
+      .joins(:comments)
+      .where("comments.created_at > ?", Time.zone.today - 7)
+      .group(:name)
+      .order("comments_count DESC")
+      .limit(10)
   end
 end
